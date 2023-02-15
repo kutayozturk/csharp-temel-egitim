269ba82762e3017171c07e94959829b59d7e30ef
Kutay ÖZTÜRK <94574681+kutayozturk@users.noreply.github.com>
2023-02-16 00:27:19 +0300

    Update 02 - Ekran Çıktısı ve Veri Alma Komutları.cs

--- 02 - Ekran Çıktısı ve Veri Alma Komutları.cs
+++ 02 - Ekran Çıktısı ve Veri Alma Komutları.cs
@@ -1,12 +1,13 @@
 
 // Ekrana Yaz Yazdırma)
+
 	Console.WriteLine("Merhaba Dünya");
 	Console.Write(Merhaba Dünya); // Alt satıra inmez
 
 // Kullanıcıdan Veri Alma
 	
-Console.ReadLine(); // Çok karakterli veri girişi yapmaya yarar
-Console.ReadKey(); 	// Tek karakterli veri girişi yapmaya yarar
+	Console.ReadLine(); // Çok karakterli veri girişi yapmaya yarar
+	Console.ReadKey(); 	// Tek karakterli veri girişi yapmaya yarar
 
 
 
