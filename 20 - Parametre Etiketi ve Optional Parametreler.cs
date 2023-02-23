e55ca40fd1a6b6454e1e4b8183359f392249e66c
Kutay ÖZTÜRK <94574681+kutayozturk@users.noreply.github.com>
2023-02-23 19:55:28 +0300

    Update 20 - Parametre Etiketi ve Optional Parametreler.cs

--- 20 - Parametre Etiketi ve Optional Parametreler.cs
+++ 20 - Parametre Etiketi ve Optional Parametreler.cs
@@ -10,17 +10,23 @@ namespace ConsoleApp1
     {
         private static void Main(string[] args)
         {
-            char[] karakterDizim = KarakterleriVer(sehir:"Paris",ulke:"Fransa");
-            /* 
-            Yukarıda yer alan (sehir:"Paris",ulke:"Fransa"); kısmına NamedPrameter denir. 
+            Console.WriteLine("Ülke Gir: ");
+            string ulke = Console.ReadLine();
+
+            Console.WriteLine("Şehir Gir: ");
+            string sehir = Console.ReadLine();
+
+            char[] karakterDizim = KarakterleriVer(sehir: sehir, ulke: ulke);
+            /*
+            Yukarıda yer alan (sehir:"Paris",ulke:"Fransa"); kısmına NamedPrameter denir.
             Parametre ismini belirterek değer göndermemizi sağladı.
 
             Optional Parameter (Opsiyonel Parametre): Parametre değeri gönderilmezse, daha önceden belirlediğimiz bir paramatreyi
             varsayılan değer olarak gönderebiliriz.
-            
+
             ---
                 char[] karakterDizim = KarakterleriVer(ulke: "Fransa");
-            
+
             Yukarıda yer alan kod parçasında görüldüğü gibi, "sehir" paramtresi gönderilmemiştir.
             "ulke" parametresi gönderilirken "ulke" etiketi ile gönderildiğinden, sehir ANKARA olarak kalır, ülke Fransa olarak
             değişecektir.
@@ -41,8 +47,8 @@ namespace ConsoleApp1
             Console.ReadLine();
         }
 
-        private static char[] KarakterleriVer(string ulke="Türkiye", string sehir = "Ankara")
-            // Yukarıda Optional Parameter kullanışmıştır. "=" operatörü ile varsayılan değerler eklenmiştir.
+        private static char[] KarakterleriVer(string ulke = "Türkiye", string sehir = "Ankara")
+        // Yukarıda Optional Parameter kullanışmıştır. "=" operatörü ile varsayılan değerler eklenmiştir.
         {
             char[] karakterler = new char[ulke.Length + sehir.Length];
             for (int i = 0; i < ulke.Length; i++)
