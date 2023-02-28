b40aa702bc7b9366a931794e70d56ffb4532d721
Kutay ÖZTÜRK <94574681+kutayozturk@users.noreply.github.com>
2023-03-01 02:10:36 +0300

    Add files via upload

---
+++ 26 - class Örneği.cs
@@ -0,0 +1,55 @@
+//	Class Örneği
+//	--------------- Program.cs ---------------
+using System;
+
+namespace CLASS
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Ogrenci ogr1 = new Ogrenci();
+            Ogrenci ogr2 = new Ogrenci();
+
+            ogr1.yas = 15;
+            ogr1.isim = "Tayfun";
+
+            ogr2.yas = 25;
+            ogr2.isim = "Kutay";
+
+            ogr1.yazdir();
+            ogr2.yazdir();
+
+            Console.ReadKey();
+        }
+    }
+}
+
+//	--------------- Ogrenci.cs ---------------
+
+using System;
+
+namespace CLASS
+{
+    internal class Ogrenci
+    {
+        public int yas;
+        public string isim;
+
+        public void yazdir()
+        {
+            Console.WriteLine("{0}.....{1}", yas, isim);
+        }
+    }
+}
+
+// NOT: 
+	Ogrenci ogr4;
+	ogr4.yazdir();
+	
+// Şeklinde kullanamayacağımızı daha önce belirtmiştik. Visual Studio zaten hata verecektir.
+// new operatörü kullanıp bir nesne oluşturmadığımız için.
+
+// Bu şekilde bir referans oluşturmak aslında.
+	Ogrenci ogr4 = null;
+// ifadesi ile aynı şeydir.
