22034d78ecde6321e2d721d18118660287c13601
Kutay ÖZTÜRK <94574681+kutayozturk@users.noreply.github.com>
2023-03-01 02:34:19 +0300

    Create 027 - class Örneği (Referanssız).cs

---
+++ 027 - class Örneği (Referanssız).cs
@@ -0,0 +1,46 @@
+//	Class Farklı Kullanımı
+
+//	--------------- Ogrenci.cs ---------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASS
+{
+    internal class Ogrenci
+    {
+        public int yas;
+        public string isim;
+
+        public void yazdir()
+        {
+            Console.WriteLine("{0}.....{1}", yas, isim);
+        }
+    }
+}
+
+//	--------------- Program.cs ---------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASS
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            (new Ogrenci()).yazdir(); // Referansı olmayan bir nesne oluşturduk ve bu nesneyş kullandık.
+            Console.ReadKey();
+        }
+    }
+}
+
+// Ekran Çıktısı:	0.....
+
+
